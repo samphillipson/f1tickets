3575b741c4646564fb777c2f2a6da944e972a7bc
samphillipson <s267450@uos.ac.uk>
2025-03-10 17:21:29 +0000

    edited

--- f1tickets/f1tickets/settings.py
+++ f1tickets/f1tickets/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'f1ticketsapp', 'static')]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 
